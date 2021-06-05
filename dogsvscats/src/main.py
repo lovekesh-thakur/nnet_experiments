5be51fa6dabef05c879671845d39f9a66d12c850
Lovekesh Bhati <love.aiesec@gmail.com>
2021-06-05 12:31:59 +0530

    added checkpoint save and load, saving best model and learning rate decay on plateau

--- dogsvscats/src/main.py
+++ dogsvscats/src/main.py
@@ -9,6 +9,7 @@ from torch.utils.data import DataLoader
 import torch
 from tqdm import tqdm
 from config import *
+import json
 
 import wandb
 
@@ -17,7 +18,8 @@ wandb.login()
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 conf = adam_config
-
+resume_training = False
+resume_checkpoint = None
 
 
 train_transform = A.Compose(
@@ -39,6 +41,7 @@ valid_transform = A.Compose(
                             ]
                             )
 
+### Dataset Loader
 train_dataset = datasets.DogsVsCatsDataset("/home/lovekesh/Developer/nnet_experiments/dogsvscats/data/train.txt", 
                             transforms=train_transform)
 train_data = DataLoader(dataset=train_dataset, batch_size=conf['BATCH_SIZE'], num_workers=4, shuffle=True)
@@ -47,16 +50,34 @@ valid_dataset = datasets.DogsVsCatsDataset("/home/lovekesh/Developer/nnet_experi
                             transforms=valid_transform)
 valid_data = DataLoader(dataset=train_dataset, batch_size=conf['BATCH_SIZE'], num_workers=4, shuffle=True)
 
+### preparing model and loss function
 net = resnet.Resnet34().to(device=device)
 loss_fn = nn.CrossEntropyLoss()
+
+## setting up optimizer
 if conf['optimizer'] == 'sgd':
     optimizer = optim.SGD(net.parameters(), lr=conf['learning_rate'], momentum=0.9)
 elif conf['optimizer'] == 'Adam':
     optimizer = optim.Adam(net.parameters(), lr = conf['learning_rate'])
 
+if resume_training:
+    checkpoint = torch.load(resume_checkpoint)
+    net.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    start_epoch = checkpoint['epoch']
+else:
+    start_epoch = 0
+
+
+## setting up learning rate scheduler
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, verbose=True)
+
+best_val_loss = 10000
+
+## running training and logging with wandb
 with wandb.init(project = 'resnet34-classifier', config = conf):
     start_iterations = 0
-    for i in range(500):
+    for i in tqdm(range(start_epoch, 500)):
         
         stats = trainer.train(net, train_data, optimizer, loss_fn, device=device)
 
@@ -65,12 +86,29 @@ with wandb.init(project = 'resnet34-classifier', config = conf):
         
         
         validation_stats = trainer.valid(net, valid_data, loss_fn, device=device)
+        
         wandb.log(validation_stats, step = start_iterations)
         
-        """
-        if (i + 1) % conf['EVAl_AFTER_EPOCHS'] == 0:
-            
-            torch.save(net, f"./../weights/model_{(i + 1) * conf['EVAl_AFTER_EPOCHS']}.pth")
+        scheduler.step(validation_stats['val_loss'])
 
-            net = torch.load(f"./../weights/model_{(i + 1) * conf['EVAl_AFTER_EPOCHS']}.pth").to(device)
-        """
+        ### saving best model
+
+        if validation_stats['val_loss'] < best_val_loss:
+            best_val_loss = validation_stats['val_loss']
+            torch.save(net, "./../weights/model_best.pth") # saved model
+
+            validation_stats['epoch'] = i + 1
+            json_object = json.dumps(validation_stats, indent = 4)
+            with open("./../weights/model_best.json", "w") as outfile: # saved json containing metadata about best model
+                json.dump(validation_stats, outfile)
+        
+        
+        if (i + 1) % conf['EVAl_AFTER_EPOCHS'] == 0:
+
+            torch.save({
+                        'epoch': i,
+                        'model_state_dict': net.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        'loss': stats['loss'],
+                        }, f"./../weights/model_{(i + 1)}.pth")
+                    
